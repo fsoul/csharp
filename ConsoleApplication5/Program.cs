1cc5545a3e5ee07be0a6f1e400881a3f880266f7
fsoul <bilinskyivitalii@gmail.com>
2016-06-26 15:32:42 +0300

    update

--- ConsoleApplication5/Program.cs
+++ ConsoleApplication5/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApplication5
 {
@@ -10,6 +11,39 @@ namespace ConsoleApplication5
     {
         static void Main(string[] args)
         {
+            string path = "input.txt";
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string line = sr.ReadToEnd();
+                    char[] str = line.ToCharArray();
+                    int max = 0;
+                    int c = 0;
+                    for (int i = 0; i < str.Length; i++)
+                    {
+                        if (str[i] == '1')
+                        {
+                            c++;
+                        }
+                        else
+                        {
+                            c = 0;
+                        }
+                        if (c > max)
+                        {
+                            max = c;
+                        }
+                    }
+                    Console.WriteLine(max);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
